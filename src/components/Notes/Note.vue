26daf4f3260449b5518dfd648f2391c59414eb46
Arman Legryan <arman.legryan@exio.tech>
2022-11-28 22:56:22 +0400

    Created AddEditNote.vue component and NotesView.vue view

--- src/components/Notes/Note.vue
+++ src/components/Notes/Note.vue
@@ -9,7 +9,12 @@
       </div>
     </div>
     <footer class="card-footer">
-      <a href="#" class="card-footer-item">Edit</a>
+      <RouterLink
+        :to="{ name: 'edit-note', params: { id: note.id } }"
+        class="card-footer-item"
+      >
+        Edit
+      </RouterLink>
       <a
         href="#"
         class="card-footer-item"
--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -1,29 +1,16 @@
 <template>
   <div class="notes">
-    <div class="has-background-success-dark mb-5 p-4">
-      <div class="field">
-        <div class="control">
-          <textarea
-            v-model.trim="newNote"
-            class="textarea"
-            placeholder="Add a new note"
-            ref="newNoteRef"
-          />
-        </div>
-      </div>
-
-      <div class="field is-grouped is-grouped-right">
-        <div class="control">
-          <button
-            class="button is-link has-background-success"
-            :disabled="!newNote"
-            @click="addNote"
-          >
-            Add a New Note
-          </button>
-        </div>
-      </div>
-    </div>
+    <AddEditNote v-model="newNote">
+      <template #buttons>
+        <button
+          class="button is-link has-background-success"
+          :disabled="!newNote"
+          @click="addNote"
+        >
+          Add a New Note
+        </button>
+      </template>
+    </AddEditNote>
 
     <Note v-for="note in storeNotes.notes" :key="note.id" :note="note" />
   </div>
@@ -31,8 +18,9 @@
 
 <script setup>
 import { ref } from "vue";
-import Note from "@/components/Notes/Note.vue";
 import { useStoreNotes } from "@/stores/storeNotes";
+import Note from "@/components/Notes/Note.vue";
+import AddEditNote from "@/components/Notes/AddEditNote.vue";
 
 const newNote = ref("");
 const newNoteRef = ref(null);
